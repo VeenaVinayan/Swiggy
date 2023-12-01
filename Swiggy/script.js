c78e891723a9adbe106950926d7ff3311cb8f8de
VeenaVinayan <85392700+VeenaVinayan@users.noreply.github.com>
2023-12-01 09:53:35 +0530

    Add files via upload

---
+++ Swiggy/script.js
@@ -0,0 +1,10 @@
+function displayDiv(){
+    console.log("Hai")
+    document.getElementsById("hidden-div").style.display="block";
+    
+}
+
+function hideDiv(){
+    document.getElementsById("hidden-div").style.display='"none"; 
+    console.log("Hello")
+}
